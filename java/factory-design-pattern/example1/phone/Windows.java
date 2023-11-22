0cc5864252d1f691f7f452721c193c5f5e94f427
ZamaaN <zamaan.md19@gmail.com>
2023-11-23 01:32:49 +0600

    Create Windows.java

---
+++ java/factory-design-pattern/example1/phone/Windows.java
@@ -0,0 +1,11 @@
+package Factory_Design_Pattern.Example1.phone;
+
+public class Windows implements OS
+{
+    @Override
+    public void spec()
+    {
+        System.out.println("I am the most popular in PCs... ...");
+
+    }
+}
