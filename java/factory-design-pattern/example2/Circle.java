e442eebc3250998a968109d4852d8ea4e79ec165
ZamaaN <zamaan.md19@gmail.com>
2023-11-23 01:36:05 +0600

    Create Circle.java

---
+++ java/factory-design-pattern/example2/Circle.java
@@ -0,0 +1,7 @@
+package Factory_Design_Pattern.Example2;
+public class Circle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle:: draw() method");
+    }
+}
